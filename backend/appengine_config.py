8ff0541b53271c464973b007f4a57c1703bfce66
Adrian Corduneanu <adrian.dc@gmail.com>
2017-04-28 23:29:13 -0700

    webhook working

--- backend/appengine_config.py
+++ backend/appengine_config.py
@@ -2,3 +2,18 @@ from google.appengine.ext import vendor
 
 # Add any libraries installed in the "lib" folder.
 vendor.add('lib')
+
+##
+## Enable ctypes on dev appserver so we get proper flask tracebacks. From http://jinja.pocoo.org/docs/dev/faq/#my-tracebacks-look-weird-what-s-happening
+## and http://stackoverflow.com/questions/3086091/debug-jinja2-in-google-app-engine
+import os
+PRODUCTION_MODE = not os.environ.get(
+    'SERVER_SOFTWARE', 'Development').startswith('Development')
+if not PRODUCTION_MODE:
+    from google.appengine.tools.devappserver2.python import sandbox
+    sandbox._WHITE_LIST_C_MODULES += ['_ctypes', 'gestalt']
+    import os
+    import sys
+    if os.name == 'nt':
+        os.name = None
+        sys.platform = ''
--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,71 @@
 """`main` is the top level module for your Flask application."""
+from __future__ import print_function # In python 2.7
+import json
+import sys
+from google.appengine.ext import ndb
 
 # Import the Flask Framework
-from flask import Flask
+from flask import Flask, request, make_response
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+class GoogleUser(ndb.Model):
+    """A Google Home user. The key name is the user id"""
+    pass
+
+class Word(ndb.Model):
+    """A word to be learned. The key name is the word itself"""
+    learned = ndb.BooleanProperty()
+    definition = ndb.StringProperty(indexed=False)
+
+
+@app.route('/apiai', methods=['POST'])
+def apiai_webhook():
+    """Processes Api.ai webhooks"""
+    req = request.get_json(silent=True, force=True)
+
+    print('Request:', file=sys.stderr)
+    print(json.dumps(req, indent=4), file=sys.stderr)
+
+    res = make_webhook_result(req)
+
+    res = json.dumps(res, indent=4)
+    print('Response:', file=sys.stderr)
+    print(res, file=sys.stderr)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def make_webhook_result(req):
+    action = req.get('result').get('action')
+    parameters = req.get('result').get('parameters')    
+    speech = process_action(action, parameters)
+
+    return {
+        'speech': speech,
+        'displayText': speech,
+        #"data": {},
+        # "contextOut": [],
+        'source': 'apiai-wordlist'
+    }
+
+
+def process_action(action, params):
+    """Processes an action with the parameters dictionary, and returns
+    the speech reply to be spoken to the user."""
+    if action == 'define_word':
+        word = params.get('word')
+        if word:
+            return 'I do not understand the word %s' % word
+        else:
+            return 'I did not get that'
+    elif action == 'count_words':
+        pass
+    
+    return 'I did not get that'
 
 
 @app.errorhandler(404)
